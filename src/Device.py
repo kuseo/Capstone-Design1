e762ccb9da23c0c763a151497d4a6e652bc5dab7
kanguk Seo <tjrkddnr@korea.ac.kr>
2019-05-18 01:23:42 +0900

    Use dictionary instead of class

--- src/Device.py
+++ src/Device.py
@@ -1,23 +1,19 @@
-import FeatureEngineering as fe
-
 class Device(object):
     def __init__(self):
         self.packet_info = []
         self.feature_data = []
 
     def append_packet_info(self, packet_info):
-        temp = fe.PacketInfo(packet_info)
-        self.packet_info.append(temp)
+        self.packet_info.append(packet_info)
     
     def append_feature_data(self, feature_data):
-        temp = fe.FeatureData(feature_data)
-        self.feature_data.append(temp)
+        self.feature_data.append(feature_data)
 
     # count number of destinations
     def count_endpoint(self):
         endpoint = []
         for value in self.packet_info:
-            endpoint.append(value.destination)
+            endpoint.append(value['destination'])
         endpoint = list(set(endpoint)) # delete overlapped data
 
         return len(endpoint)
