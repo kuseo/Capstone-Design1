b15a653aa3462d4a27031e0ffdb3a5b1d7212797
서강욱 <tjrkddnr@korea.ac.kr>
2019-05-17 14:27:22 +0900

    resolve first packet delta error

--- src/pcapAnalyzer.py
+++ src/pcapAnalyzer.py
@@ -27,7 +27,6 @@ if __name__ == "__main__":
             'protocol':value.protocol,
             'destination':value.destination,
         }
-        
         IP = value.source
         if IP in device:
             device[IP].append_packet_info(packet)
@@ -40,49 +39,41 @@ if __name__ == "__main__":
         num_of_endpoint = device[source_IP].count_endpoint()
 
         # packet info -> feature data
-        index = 0
-        for value in device[source_IP].packet_info:
+
+        #very first packet
+        parameters = {
+            'size':int(device[source_IP].packet_info[0].size),
+            'one_hot_protocol':one_hot_protocol[device[source_IP].packet_info[0].protocol],
+            'bandwidth':0.0,
+            'endpoint':num_of_endpoint,
+            'T1':0.0,
+            'T2':0.0,
+            'T3':0.0,
+        }
+        device[source_IP].append_feature_data(parameters)
+
+        index = 1
+        for value in device[source_IP].packet_info[1:]:
             parameters = {
                 'size':int(value.size),
-                'T1':0,
-                'T2':0,
-                'T3':0,
                 'one_hot_protocol':one_hot_protocol[value.protocol],
-                'bandwidth':0,
+                'bandwidth':0.0,
                 'endpoint':num_of_endpoint,
             }
             # delta time, get T1
-            try:
-                parameters['T1'] = float(device[source_IP].packet_info[index].time) - float(device[source_IP].packet_info[index - 1].time)
-            except IndexError:
-                parameters['T1'] = 0.0
+            parameters['T1'] = float(device[source_IP].packet_info[index].time) - float(device[source_IP].packet_info[index - 1].time)
+            
+            # delta T1, get T2
+            parameters['T2'] = parameters['T1'] - device[source_IP].feature_data[index-1].T1
 
-            device[source_IP].append_feature_data(parameters)
-
-            index = index + 1
+            # delta T2, get T3
+            parameters['T3'] = parameters['T2'] - device[source_IP].feature_data[index-1].T2
         
-        # delta T1, get T2
-        index = 0
-        for value in device[source_IP].feature_data:
-            try:
-                value.T2 = device[source_IP].feature_data[index].T1 - device[source_IP].feature_data[index-1].T1
-            except IndexError:
-                value.T2 = 0.0
+            device[source_IP].append_feature_data(parameters)
             index = index + 1
-
-        # delta T2, get T3
-        index = 0
-        for value in device[source_IP].feature_data:
-            try:
-                value.T3 = device[source_IP].feature_data[index].T2 - device[source_IP].feature_data[index-1].T2
-            except IndexError:
-                value.T3 = 0.0
-            index = index + 1
-
-
-
-    #print(device['38.198.26.9'].feature_data[0].protocol)
-
+        # First 3 packets have abnormal feature data for T1, T2, T3 since  they are initialized to 0.0
+        # But our ANN will be robust. so It's OKay.
+    
     # Test : loop over all feature datas
     count_device = 0
     count_feature_data = 0
@@ -93,3 +84,4 @@ if __name__ == "__main__":
             count_feature_data = count_feature_data + 1
             print("\tpacket number " + str(count_feature_data) + ": " + str(value.size) + "\t" + str(value.T1)  + "\t" + str(value.T2) + "\t" + str(value.T3) + "\t" + str(value.protocol) + "\t" + str(value.bandwidth) + "\t" + str(value.endpoint))
     print(count_device, count_feature_data)
+    
