416f67b954effb9b6a97cc6ba32a106e28a25a7a
kanguk Seo <tjrkddnr@korea.ac.kr>
2019-05-28 02:33:12 +0900

    some test

--- src/test/test.py
+++ src/test/test.py
@@ -4,3 +4,12 @@ device_score = dict(pair)
 print(device_score)
 # device_score would be like
 # { "11.22.33.44":0, "12.34.56.78":0, "99.88.77.66":0, ... }
+
+li = [2,4,6,8]
+it = iter(li)
+for v in range(next(it), next(it)):
+    print(v)
+
+li = [1]
+for v in li[0:0]:
+    print(v)
